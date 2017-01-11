a1b84d8f13932c4b993729455d078ae51bf97a92
ravikantsv <ravikantsv@yahoo.com>
2017-01-11 02:35:31 -0800

    Added the AlertDialog Builder to display popup options

--- app/src/main/java/com/samatham/popwindowtest/MainActivity.java
+++ app/src/main/java/com/samatham/popwindowtest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.samatham.popwindowtest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -27,5 +30,22 @@ public class MainActivity extends Activity implements OnClickListener {
         if (tipWindow != null && tipWindow.isTooltipShown())
             tipWindow.dismissTooltip();
         super.onDestroy();
+    }
+
+    public void dispPopupStack(View view) {
+        CharSequence colors[] = new CharSequence[] {"Measure", "Rename", "Compare", "Delete", "Mole was removed", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Modify Mole");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // the user clicked on colors[which]
+                if (which == 3){
+                    Toast.makeText(MainActivity.this,"You clicked yes button",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        builder.show();
     }
 }
